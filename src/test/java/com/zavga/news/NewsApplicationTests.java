9e535dc78f8b786fc74255ea3351c358563b7a31
zavgarinho <zavgardan@gmail.com>
2025-12-22 22:05:30 +0100

    change test

--- src/test/java/com/zavga/news/NewsApplicationTests.java
+++ src/test/java/com/zavga/news/NewsApplicationTests.java
@@ -2,8 +2,10 @@ package com.zavga.news;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NewsApplicationTests {
 
 	@Test
